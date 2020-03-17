fadb9a1ad12914915c507bdabe3df7dac9c747d9
Joost Koopmans <joost@teaminova.nl>
2020-03-17 12:04:54 +0100

    cs patch

--- src/Attribute.php
+++ src/Attribute.php
@@ -27,12 +27,23 @@ class Attribute extends Model
      * @{inheriteDoc}
      */
     protected $fillable = [
-        'attribute_code', 'backend_class', 'backend_type',
-        'backend_table', 'frontend_class', 'frontend_type',
-        'frontend_label', 'source_class', 'default_value',
-        'is_filterable', 'is_searchable', 'is_required',
-        'required_validate_class', 'entity_id',
-        'is_multiple', 'is_filterable_admin'
+        'attribute_code',
+        'backend_class',
+        'backend_type',
+        'backend_table',
+        'frontend_class',
+        'frontend_type',
+        'frontend_label',
+        'source_class',
+        'default_value',
+        'is_filterable',
+        'is_searchable',
+        'is_required',
+        'required_validate_class',
+        'entity_id',
+        'is_multiple',
+        'is_filterable_admin',
+        'form_container',
     ];
 
     /**
